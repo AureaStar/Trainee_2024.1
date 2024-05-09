4fd1c8fc409f73fabc3af85d6886b423229c7622
Artur Castro <arturcastro010@gmail.com>
2024-05-09 14:06:31 -0300

    carrossel em progresso

--- public/js/landing-page.js
+++ public/js/landing-page.js
@@ -1,4 +1,14 @@
-const cposts = document.querySelector("total");
+const cposts = document.querySelector(".c-posts");
+const firstPost = document.querySelectorAll(".post-mobile")[0];
+const arrows = document.querySelectorAll(".carousel i");
+
+/*arrows.forEach(arrow => {
+    arrow.addEventListener("click", () => {
+        console.log(arrow);
+    });
+});*/
+
+
 
 
 
