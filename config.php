566d6274fb6dd98d5eae007a9b6c82f226c55eb3
AureaStar <aurea.starlight@gmail.com>
2024-05-26 17:39:35 -0300

    Migragação do Repositório PHP

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'database' => [
+        'name' => 'torre_do_sabio',
+        'username' => 'root',
+        'password' => '',
+        'connection' => 'mysql:host=127.0.0.1',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    ]
+];
