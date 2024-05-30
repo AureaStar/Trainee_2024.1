018cccdc3e6dac819a06ff9c06e14799368f741e
Esthefany <esthefanyemmanuelesc@gmail.com>
2024-05-30 16:07:23 -0300

    .

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
     'database' => [
-        'name' => 'torre_do_sabio',
+        'name' => 'biblioteca_do_sabio',
         'username' => 'root',
         'password' => '',
         'connection' => 'mysql:host=127.0.0.1',
