ad541b476ef12527ba43bf0076529f9cf3d49ed4
Artur Castro <arturcastro010@gmail.com>
2024-06-09 21:41:27 -0300

    botão de voltar no post individual, CSS WIP

--- app/views/site/post-individual.view.php
+++ app/views/site/post-individual.view.php
@@ -8,6 +8,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Almendra:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Almendra:ital,wght@0,400;0,700;1,400;1,700&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
     <link rel="stylesheet" href="/public/css/post-individual.css">
     <link rel="shortcut icon" type="imagex/png" href="/public/assets/torre-logo.ico">
@@ -21,6 +23,11 @@
             <div class="autoria">
                 <img class="autoria-perfil" src="/public/assets/xadrez.jpeg" alt="Foto de perfil">
                 <p>Escrito por Username em XX/XX/XXXX</p>
+            </div>
+            <div class="IndexButton">
+                <form method="get" action="posts">
+                    <button type="submit" class="material-icons icone">undo</button>
+                </form>
             </div>
             <div class="box-text">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut mauris gravida, maximus tellus id, dictum urna. Cras ornare tellus justo, sit amet maximus sapien maximus dictum. Mauris velit justo, suscipit ac leo sit amet, ornare finibus felis. Donec felis leo, euismod sed ultricies in, maximus quis nulla. Vestibulum sed congue mi. Praesent eleifend viverra ipsum, ac aliquet est euismod et. In et pharetra est, eget convallis nulla. In hac habitasse platea dictumst. Etiam ullamcorper sed est ac vestibulum. Aliquam sit amet quam ultrices elit dapibus cursus. Praesent elit lacus, suscipit quis ante non, sagittis tristique diam. Praesent vestibulum quam sit amet ipsum sodales, et eleifend ligula pharetra. </p>
