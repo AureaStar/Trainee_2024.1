728ca6a412afd4eb9180a441288dd25614b3a292
Caio Reis <caiofreis2005@gmail.com>
2024-06-11 14:06:43 -0300

    Corrigindo modal

--- public/js/modal.js
+++ public/js/modal.js
@@ -3,10 +3,16 @@ function fechaModal(){
     close.forEach(modal=>{
         modal.style.display = 'none';
     });
+
+    let overlay = document.querySelector('.overlay');
+    overlay.style.display = 'none';
 }
 
 function abreModal(idModal){
     fechaModal();
     let open = document.getElementById(idModal);
     open.style.display = 'block';
+
+    let overlay = document.querySelector('.overlay');
+    overlay.style.display = 'block';
 }
