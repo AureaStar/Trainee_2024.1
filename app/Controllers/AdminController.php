9c70133a0ffaf95df5f0e1013f51234095050901
Caio Reis <caiofreis2005@gmail.com>
2024-06-11 14:07:39 -0300

    Adicionando caminho da imagem

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -18,9 +18,16 @@ class AdminController
 
     public function create()
     {
+        $temporario = $_FILES['imagem']['tmp_name'];
+        $nomeimagem = $_FILES['imagem']['name'];
+        $destinoimagem = "../../htdocs/Trainee_2024.1/public/imagens/";
+        move_uploaded_file($temporario, $destinoimagem . $nomeimagem);
+        $caminhodaimagem = "../../public/imagens/" . $nomeimagem;
+
         $parameters = [
             'title' => $_POST['title'],
             'content' => $_POST['text'],
+            'author' => 1,
             'category' => $_POST['categorias'],
             'created_at' => $_POST['data'],
             'image' => $_POST['imagem']
