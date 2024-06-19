462e6db33f776ad1502959a100e3a7468f02723a
Esthefany <esthefanyemmanuelesc@gmail.com>
2024-06-18 21:17:34 -0300

    apagando coisas

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -7,31 +7,7 @@ use Exception;
 
 class UserController
 {
-    public function login()
-    {
-        if(isset($_POST['email']) || isset($_POST['password'])) {
-            
-            // $email = $_POST['email'];
-            // $password = $_POST['password'];
-
-            // App::get('database')->login($email, $password);
-            // header("Location: /landing-page");
-            // return 0;
-
-            $parameters = [
-                'email' => $_POST['email'],
-                'password' => $_POST['senha'],
-            ];
-            App::get('database')->login('users', $parameters);
-            header("Location: /landing-page");
-            return 0;
-        }
-
-        session_start();
-        $_SESSION['erroLogin'] = true;
-        header("Location: /login");
-    }
-    public function criar()
+      public function criar()
     {
         if(App::get('database')->verificaEmail($_POST['email']) == false){
             $temporario = $_FILES['imagem']['tmp_name'];
--- app/views/site/login.view.php
+++ app/views/site/login.view.php
@@ -12,9 +12,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
     <link rel="stylesheet" href="../../../public/css/login.css" />
-    <link rel="stylesheet" href="/public/css/lista-de-usuarios.css">
-    <link rel="stylesheet" href="../../../public/css/excluitUsuario.css">
-
 </head>
 
 <body>
