7c3d6f364a49fd0f1a12e374d76075f4eca5b20b
Esthefany <esthefanyemmanuelesc@gmail.com>
2024-06-18 22:16:05 -0300

    criando

--- app/views/site/login.view.php
+++ app/views/site/login.view.php
@@ -17,6 +17,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
     <link rel="stylesheet" href="../../../public/css/login.css"/>
+    <link rel="stylesheet" href="../../../public/css/excluitUsuario.css">
+
 </head>
 <body>
 <?php
